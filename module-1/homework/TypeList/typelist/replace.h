7b7ca391afc5c992c8387ad8630f3abe5043c991
JijaLoqie <kras2001.www@mail.ru>
2020-12-23 22:25:07 +0300

    TypeList

--- module-1/homework/TypeList/typelist/replace.h
+++ module-1/homework/TypeList/typelist/replace.h
@@ -4,3 +4,18 @@
 
 template<typename TList, typename OldType, typename NewType> 
 struct Replace;
+
+template <typename Old, typename New>
+struct Replace<NullType, Old, New> {
+    typedef NullType NewTypeList;
+};
+
+template <typename Tail, typename Old, typename New>
+struct Replace<TypeList<Old, Tail>, Old, New> {
+    typedef TypeList<New, Tail> NewTypeList;
+};
+
+template <typename Head, typename Tail, typename Old, typename New>
+struct Replace<TypeList<Head, Tail>, Old, New> {
+    typedef TypeList<Head, typename Replace<Tail, Old, New>::NewTypeList> NewTypeList;
+};
